e4939a798562bfb104b608260eb138df4f30e1f4
Louis de Launay <161574610+delanaloui@users.noreply.github.com>
2024-02-28 10:24:11 +0200

    Create code.cpp

---
+++ code.cpp
@@ -0,0 +1,36 @@
+
+int foo_stub()
+{
+    std::cout << "I am foo_stub" << std::endl;
+    return 0;
+}
+
+int printf_stub(const char * format, ...)
+{
+    std::cout<< "I am printf_stub" << std::endl;
+    return 0;
+}
+
+int main(int argc, char **argv)
+{#include<iostream>
+#include "stub.h"
+using namespace std;
+int foo(int a)
+{   
+    cout<<"I am foo"<<endl;
+    return 0;
+}
+int foo_stub(int a)
+{   
+    cout<<"I am foo_stub"<<endl;
+    return 0;
+}
+
+
+int main()
+{
+    Stub stub;
+    stub.set(foo, foo_stub);
+    foo(1);
+    return 0;
+}
